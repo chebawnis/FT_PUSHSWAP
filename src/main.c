bb158a219c442d94d9349d94b165af5ee8f28be6
Awnis Dichou <adichou@c1r3p9.42perpignan.fr>
2025-02-23 16:52:14 +0100

    creation et remplissage de la liste chainee

--- src/main.c
+++ src/main.c
@@ -6,33 +6,41 @@
 /*   By: adichou <adichou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 07:40:58 by adichou           #+#    #+#             */
-/*   Updated: 2025/02/23 15:13:54 by adichou          ###   ########.fr       */
+/*   Updated: 2025/02/23 16:51:31 by adichou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 
 typedef struct s_struct
 {
 	int										data;
 	int										index;
-	t_struct								*next;
+	struct s_struct							*next;
 } t_struct;
 
 void	create_last_node(int data, int index, t_struct **head)
 {
+	t_struct *new_node;
 	t_struct *tmp;
 
-	tmp = *head;
-	tmp = malloc(sizeof(t_struct));\
-	if (!tmp)
+	new_node = malloc(sizeof(t_struct));
+	if (!new_node)
 		return ;
-	while (tmp)
+	new_node->data = data;
+	new_node->index = index;
+	new_node->next = NULL;
+	if (!*head)
+	{
+		*head = new_node;
+		return;
+	}
+	tmp = *head;
+	while (tmp->next)
 		tmp = tmp->next;
-	tmp->data = data;
-	tmp->index = index;
-	tmp->next = NULL;
+	tmp->next = new_node;
 }
 
 void	create_first_node(int data, int index, t_struct **head)
@@ -40,6 +48,8 @@ void	create_first_node(int data, int index, t_struct **head)
 	t_struct *tmp;
 
 	tmp = malloc(sizeof(t_struct));
+	if (!tmp)
+		return;
 	tmp->data = data;
 	tmp->index = index;
 	tmp->next = *head;
@@ -49,30 +59,36 @@ void	create_first_node(int data, int index, t_struct **head)
 
 void	fill_list(int argc, char **argv, t_struct **head)
 {
+	int	i;
+
+	i = 0;
+	if (argc < 2)
+		return;
+	while (i < argc - 1)
+	{
+		create_last_node(atoi(argv[i + 1]), i, head);
+		i ++;
+	}
+}
+
+
+void	print_list(t_struct **head)
+{
 	t_struct	*tmp;
 
 	tmp = *head;
-	if (argc < 2)
-		return;
-	
-	
-
-	int i = 0;
-	while (i < argc)
+	while (tmp)
 	{
-		tmp = malloc(sizeof(t_struct));
-		tmp->data = atoi(argv[i + 1]);
-		tmp->index = i;
-		tmp->next = NULL;
-		i ++;
-		if (i < argc)
-			tmp->next = tmp;
+		printf("valeur %d = %d\n", tmp->index, tmp->data);
+		tmp = tmp->next;
 	}
-	
 }
+
+
 
 int	main(int argc, char **argv)
 {
-	t_struct *node1;
+	t_struct *node1 = NULL;
 	fill_list(argc, argv, &node1);
+	print_list(&node1);
 }
