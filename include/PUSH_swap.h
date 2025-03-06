84389c19d135976819c34d0bd2c2d35cfaa3a74d
Awnis Dichou <adichou@c2r4p7.42perpignan.fr>
2025-03-06 23:27:40 +0100

    norme header

--- include/PUSH_swap.h
+++ include/PUSH_swap.h
@@ -7,18 +7,18 @@
 
 typedef struct s_struct
 {
-    int               data;
-    int               index;
-    struct s_struct   *next;
+	int					data;
+	int					index;
+	struct s_struct		*next;
 } t_struct;
 
-void    create_last_node(int data, int index, t_struct **head);
-void    create_first_node(int data, int index, t_struct **head);
-void    fill_list(int argc, char **argv, t_struct **head);
-void    rotate(t_struct **head);
-void    r_rotate(t_struct **head);
-void    swap(t_struct **head);
-void    print_list(t_struct **head);
-void    FT_PUSHSWAP(int argc, char **argv);
+void	create_last_node(int data, int index, t_struct **head);
+void	create_first_node(int data, int index, t_struct **head);
+void	fill_list(int argc, char **argv, t_struct **head);
+void	rotate(t_struct **head);
+void	r_rotate(t_struct **head);
+void	swap(t_struct **head);
+void	print_list(t_struct **head);
+void	FT_PUSHSWAP(int argc, char **argv);
 
 #endif
